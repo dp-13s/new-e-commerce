1ca91879864a313fc6f4ded65aef5dc2dcc64429
Sourav2507 <132735088+Sourav2507@users.noreply.github.com>
2024-07-26 20:45:08 +0530

    Onboarding activity added

--- app/src/main/java/com/example/myapplication/OnBoardingActivity.java
+++ app/src/main/java/com/example/myapplication/OnBoardingActivity.java
@@ -1,24 +1,92 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
 
 public class OnBoardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+    SliderAdapter sliderAdapter;
+
+    TextView[] dots;
+    Animation animation;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        //hide status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //hide toolbar
+        getSupportActionBar().hide();
+
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+        btn = findViewById(R.id.get_started_btn);
+        addDots(0);
+        viewPager.addOnPageChangeListener(changeListener);
+
+
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
     }
+
+    private void addDots(int position){
+
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+        for (int i = 0;i<dots.length;i++){
+            dots[i]=new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0){
+            dots[position].setTextColor(getResources().getColor(R.color.pink));
+        }
+    }
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            if(position == 0){
+                btn.setVisibility(TextView.INVISIBLE);
+            } else if (position == 1) {
+                btn.setVisibility(TextView.INVISIBLE);
+            }else {
+                animation = AnimationUtils.loadAnimation(OnBoardingActivity.this,R.anim.slide_animation);
+                btn.setAnimation(animation);
+                btn.setVisibility(TextView.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
