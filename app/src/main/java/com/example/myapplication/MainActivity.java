369fea978c53fe2a1c87d998f05c19658440c9a9
Sourav2507 <132735088+Sourav2507@users.noreply.github.com>
2024-07-29 02:16:42 +0530

    Added new item in Drawable

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,32 +15,17 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     AutoCompleteTextView search;
+    ImageButton search_button;
+
     TextView address;
-    ImageButton img1;
-    ImageButton img2;
-    ImageButton img3;
-    ImageButton img4;
-    ImageButton img5;
-    ImageButton img6;
-    ImageButton img7;
-    ImageButton img8;
-    ImageButton img9;
-    ImageButton img10;
-    ImageButton img11;
 
-    ImageButton image1;
-    ImageButton image2;
-    ImageButton image3;
-    ImageButton image4;
-    ImageButton image5;
-    ImageButton image6;
-    ImageButton image7;
+    ImageButton img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11;
 
-    ImageButton imge1;
-    ImageButton imge2;
-    ImageButton imge3;
-    ImageButton imge4;
-    ImageButton imge5;
+    ImageButton image1,image2,image3,image4,image5,image6,image7;
+
+    ImageButton imge1,imge2,imge3,imge4,imge5;
+
+    String sub[]={"Data Structure","Computer Architecture","DBMS","Operating System","Computer Network"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        search_button = findViewById(R.id.search_btn);
         search = findViewById(R.id.search_bar);
         address = findViewById(R.id.textView3);
         img1=findViewById(R.id.b1);
@@ -72,6 +62,19 @@ public class MainActivity extends AppCompatActivity {
         imge3=findViewById(R.id.d3);
         imge4=findViewById(R.id.d4);
         imge5=findViewById(R.id.d5);
+
+
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.select_dialog_item,sub);
+        search.setThreshold(1);
+        search.setAdapter(adapter);
 
     }
 }
