80ae57bcc8defb62b79c30062ca13c8dce5e1253
dp-13s <sadhanchpaul032@gmail.com>
2024-07-28 21:04:04 +0530

    trail

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,14 +3,10 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +23,20 @@ public class MainActivity extends AppCompatActivity {
     ImageButton img9;
     ImageButton img10;
     ImageButton img11;
+
+    ImageButton image1;
+    ImageButton image2;
+    ImageButton image3;
+    ImageButton image4;
+    ImageButton image5;
+    ImageButton image6;
+    ImageButton image7;
+
+    ImageButton imge1;
+    ImageButton imge2;
+    ImageButton imge3;
+    ImageButton imge4;
+    ImageButton imge5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +55,23 @@ public class MainActivity extends AppCompatActivity {
         img7=findViewById(R.id.b7);
         img8=findViewById(R.id.b8);
         img9=findViewById(R.id.b9);
+
         img10=findViewById(R.id.b10);
         img11=findViewById(R.id.b11);
+        image1=findViewById(R.id.c1);
+        image2=findViewById(R.id.c2);
+        image3=findViewById(R.id.c3);
+        image4=findViewById(R.id.c4);
+        image5=findViewById(R.id.c5);
+        image6=findViewById(R.id.c6);
 
+        image7 =findViewById(R.id.cat7);
 
+        imge1=findViewById(R.id.d1);
+        imge2=findViewById(R.id.d2);
+        imge3=findViewById(R.id.d3);
+        imge4=findViewById(R.id.d4);
+        imge5=findViewById(R.id.d5);
 
     }
 }
